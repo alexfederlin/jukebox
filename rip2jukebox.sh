2798f7f386b892357f232ac29cb6b68b1d2bf3f3
alexfederlin <alexander.federlin@ericsson.com>
2017-01-09 22:35:15 +0100

    add rip2jukebox.sh flow

---
+++ rip2jukebox.sh
@@ -0,0 +1,5 @@
+# rip a cd to mp3 and move to specific folder
+# create playlist and move to mpd folder
+# use glyrc (~/bin/getCover.sh) to search for cover art
+# run script to define one or several RFID(s) to connect to this playlist
+# add link RFID --> playlist to DB
