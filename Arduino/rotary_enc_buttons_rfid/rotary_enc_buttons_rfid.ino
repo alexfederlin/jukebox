183dc9f9cbe518c0ce89d30b4dc70f7c59a7c9e2
alexfederlin <alexander.federlin@ericsson.com>
2017-02-23 22:09:43 +0100

    added RFID readout to Arduino code

--- Arduino/rotary_enc_buttons_rfid/rotary_enc_buttons_rfid.ino
+++ Arduino/rotary_enc_buttons_rfid/rotary_enc_buttons_rfid.ino
@@ -1,36 +1,15 @@
 
 /*
- *      rotary_sm.ino   --  State machine implementation of rotary encoder driver
- *                          Interrupt driven; supports 2 types of interrupts:
- *                            * Polled interrupts; enable by defining TIMER2INT
- *                            * Pin change interrupts: enalbe by defining PINCHANGEINT
- *                              (Do NOT enable both at the same time)
- *
- *          This program is developed from the code at: 
- *              http://www.buxtronix.net/2011/10/rotary-encoders-done-properly.html
- *          Since this code was in the form of an arduino library, and I had unresolvable
- *          references that I could not figure out, I just modified it as a single sketch.
- *          The only library support required is if you want to use interrupt polling,
- *          and then the MsTimer2 library needs to be installed.
+ *      based on 
+ *      - Rotary Encoder: http://www.buxtronix.net/2011/10/rotary-encoders-done-properly.html
+ *      - RFID Handling:  https://gist.github.com/maniacbug/1273970
+ *      
  */
 
-/* Define PINCHANGEINT if you want to interrupt on any encoder pin change */
-//#define PINCHANGEINT
-/* --- OR --- */
-/* Define TIMER2INT if you want to use periodic interrupts to poll the encoder */
-#define TIMER2INT
 
-/* Define ENABLEPULLUPS if there are no external pull-ups on encoder AB pins */
-//#define ENABLEPULLUPS
-
-/* Define to enable the ISR debug flag */
-//#define ISRFLAG
-
-
-/* You may need to install this library (MsTimer2) from the arduino site */
-#ifdef TIMER2INT
 #include    <MsTimer2.h>
-#endif
+#include <SoftwareSerial.h>
+
 
 
 #define DIR_NONE 0x00           // No complete step yet.
@@ -38,9 +17,23 @@
 #define DIR_CCW  0x20           // Anti-clockwise step.
 
 unsigned int state;
+//button pins
+const int buttonPin_back = 3;     // the number of the pushbutton pin
+const int buttonPin_play = 4;     // the number of the pushbutton pin
+const int buttonPin_forward = 5;     // the number of the pushbutton pin
+const int ledPin =  13;      // the number of the LED pin
+
+//RotaryEncoder Pins
 unsigned int A = 8;             // pins connected to the encoder (digital_pin 2)
 unsigned int B = 9;             //              "                (digital_pin 3)
-unsigned int ISRflag = 5;       //              "                (digital_pin 3)
+unsigned int ISRflag = 6;       //              "                (digital_pin 3)
+
+//RFID Pins
+const int rfid_irq = 0;
+const int rfid_tx_pin = 2;
+const int rfid_rx_pin = 7;
+
+
          int count = 0;         // count each indent
          int old_count = 0;     // check for count changed
 const String VOLUME = "volume/";
@@ -74,11 +67,22 @@ const unsigned char ttable[8][4] = {
     {R_START,    R_START,     R_START,     R_START}                 // ILLEGAL
 };
 
+// Setup of RFID
+// For communication with RFID module
+SoftwareSerial rfid(rfid_tx_pin, rfid_rx_pin);
+ 
+// Indicates that a reading is now ready for processing
+volatile bool ready = false;
+ 
+// Buffer to contain the reading from the module
+uint8_t buffer[14];
+uint8_t* buffer_at;
+uint8_t* buffer_end = buffer + sizeof(buffer);
+ 
+void rfid_read(void);
+uint8_t rfid_get_next(void);
+////////////////////////////////
 
-const int buttonPin_back = 2;     // the number of the pushbutton pin
-const int buttonPin_play = 3;     // the number of the pushbutton pin
-const int buttonPin_forward = 4;     // the number of the pushbutton pin
-const int ledPin =  13;      // the number of the LED pin
 
 // variables will change:
 int buttonState_back = 0;         // variable for reading the pushbutton status
@@ -96,54 +100,29 @@ void setup( ) {
     pinMode(buttonPin_play, INPUT);     
     pinMode(buttonPin_forward, INPUT);  
 
-#ifdef ENABLEPULLUPS
-    digitalWrite( A, HIGH );                // set pullups
-    digitalWrite( B, HIGH );                //      "
-#endif
 
-#ifdef TIMER2INT
     MsTimer2::set( 1, T2_isr );             // interrupt polling:
     MsTimer2::start( );
-#endif
 
-#ifdef PINCHANGEINT
-    attachInterrupt( 0, AB_isr, CHANGE );   // pin-change interrupts: 
-    attachInterrupt( 1, AB_isr, CHANGE );
-#endif
-
-#ifdef ISRFLAG
-    pinMode( ISRflag, OUTPUT );             // time the ISR
-    digitalWrite( ISRflag, HIGH );          // set pull-up ON
-#endif
 
     state = (digitalRead( A ) << 1) | digitalRead( B );     // Initialise state.
     old_count = 0;
 
     Serial.begin( 9600 );
-    Serial.println( "Rotary Encoder Tests" );
+    Serial.println( "Jukebox Input Module Online" );
+    
+    // Open software serial connection to RFID module
+    pinMode(rfid_tx_pin,INPUT);
+    rfid.begin(9600);
+ 
+    // Listen for interrupt from RFID module
+    attachInterrupt(rfid_irq,rfid_read,FALLING);
+
+  
 }
 
 
-#ifdef PINCHANGEINT
-void AB_isr( ) {
-    // Grab state of input pins.
-    unsigned char pinstate = (digitalRead( A ) << 1) | digitalRead( B );
-
-    // Determine new state from the pins and state table.
-    state = ttable[state & 0x07][pinstate];
-
-    if( state & DIR_CW )    count++;
-    if( state & DIR_CCW )   count--;
-}
-#endif
-
-
-#ifdef TIMER2INT
 void T2_isr( ) {
-
-#ifdef ISRFLAG
-    digitalWrite( ISRflag, HIGH );
-#endif
 
     // Grab state of input pins.
     unsigned char pinstate = (digitalRead( A ) << 1) | digitalRead( B );
@@ -154,14 +133,13 @@ void T2_isr( ) {
     if( state & DIR_CW )    count=count+10;        // count up for clockwise
     if( state & DIR_CCW )   count=count-10;        // count down for counterclockwise
 
-#ifdef ISRFLAG
-    digitalWrite( ISRflag, LOW );
-#endif
 }
-#endif
+
 
 
 void loop( ) {
+
+// Rotary Encoder 
     if( old_count != count ) {
         if ( count > 100 ) {
           count=100;
@@ -176,6 +154,7 @@ void loop( ) {
         old_count = count;
     }
 
+//Pushbuttons
   // read the state of the pushbutton value:
   buttonState_back = digitalRead(buttonPin_back);
   buttonState_play = digitalRead(buttonPin_play);
@@ -207,5 +186,90 @@ void loop( ) {
     digitalWrite(ledPin, LOW); 
     sentstate = 0;
   }
-        
+
+//RFID
+  if ( ready )
+  {
+    // Convert the buffer into a 32-bit value
+    uint32_t result = 0;
+    
+    // Skip the preamble
+    ++buffer_at;
+    
+    // Accumulate the checksum, starting with the first value
+    uint8_t checksum = rfid_get_next();
+    
+    // We are looking for 4 more values
+    int i = 4;    
+    while(i--)
+    {
+      // Grab the next value
+      uint8_t value = rfid_get_next();
+      
+      // Add it into the result
+      result <<= 8;
+      result |= value;
+      
+      // Xor it into the checksum
+      checksum ^= value;
+    }
+    
+    // Pull out the checksum from the data
+    uint8_t data_checksum = rfid_get_next();
+    
+    // Print the result
+    //Serial.print("Reading: ");
+    //Serial.print(result);
+ 
+    if ( checksum == data_checksum )
+      Serial.println(result);
+      //Serial.println(" OK");
+    else
+      Serial.println(" CHECKSUM FAILED");
+ 
+    // We're done processing, so there is no current value    
+    ready = false;
+  }
+
+
 }
+
+//helper functions for RFID
+// Convert the next two chars in the stream into a byte and
+// return that
+uint8_t rfid_get_next(void)
+{
+  // sscanf needs a 2-byte space to put the result but we
+  // only need one byte.
+  uint16_t result;
+ 
+  // Working space to assemble each byte
+  static char byte_chars[3];
+  
+  // Pull out one byte from this position in the stream
+  snprintf(byte_chars,3,"%c%c",buffer_at[0],buffer_at[1]);
+  sscanf(byte_chars,"%x",&result);
+  buffer_at += 2;
+  
+  return static_cast<uint8_t>(result);
+}
+ 
+void rfid_read(void)
+{
+  // Only read in values if there is not already a value waiting to be
+  // processed
+  if ( ! ready )
+  {
+    // Read characters into the buffer until it is full
+    buffer_at = buffer;
+    while ( buffer_at < buffer_end )
+      *buffer_at++ = rfid.read();
+      
+    // Reset buffer pointer so it's easy to read out
+    buffer_at = buffer;
+  
+    // Signal that the buffer has data ready
+    ready = true;
+  }
+}
+
