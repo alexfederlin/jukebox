a98afff1d191697b9c80d6c5b563ac52a352a640
alexfederlin <alexander.federlin@ericsson.com>
2017-03-03 22:34:59 +0100

    added proper RFID control to Arduino JukeboxIO

--- Arduino/rotary_enc_buttons_rfid/rotary_enc_buttons_rfid.ino
+++ Arduino/rotary_enc_buttons_rfid/rotary_enc_buttons_rfid.ino
@@ -2,43 +2,31 @@
 /*
  *      based on 
  *      - Rotary Encoder: http://www.buxtronix.net/2011/10/rotary-encoders-done-properly.html
- *      - RFID Handling:  https://gist.github.com/maniacbug/1273970
+ *      - RFID Handling:  http://playground.arduino.cc/Main/RDM630RFIDReaderLibrary
  *      
  */
 
 
 #include    <MsTimer2.h>
 #include <SoftwareSerial.h>
+#include "rdm630.h"
 
 
+// Rotary Encoder Stuff
 
 #define DIR_NONE 0x00           // No complete step yet.
 #define DIR_CW   0x10           // Clockwise step.
 #define DIR_CCW  0x20           // Anti-clockwise step.
 
-unsigned int state;
-//button pins
-const int buttonPin_back = 3;     // the number of the pushbutton pin
-const int buttonPin_play = 4;     // the number of the pushbutton pin
-const int buttonPin_forward = 5;     // the number of the pushbutton pin
-const int ledPin =  13;      // the number of the LED pin
-
-//RotaryEncoder Pins
 unsigned int A = 8;             // pins connected to the encoder (digital_pin 2)
 unsigned int B = 9;             //              "                (digital_pin 3)
 unsigned int ISRflag = 6;       //              "                (digital_pin 3)
 
-//RFID Pins
-const int rfid_irq = 0;
-const int rfid_tx_pin = 2;
-const int rfid_rx_pin = 7;
-
-
+unsigned int state;
          int count = 0;         // count each indent
          int old_count = 0;     // check for count changed
 const String VOLUME = "volume/";
 String vol = VOLUME;
-
 /*
  * The below state table has, for each state (row), the new state
  * to set based on the next encoder output. From left to right in,
@@ -67,30 +55,31 @@ const unsigned char ttable[8][4] = {
     {R_START,    R_START,     R_START,     R_START}                 // ILLEGAL
 };
 
-// Setup of RFID
-// For communication with RFID module
-SoftwareSerial rfid(rfid_tx_pin, rfid_rx_pin);
- 
-// Indicates that a reading is now ready for processing
-volatile bool ready = false;
- 
-// Buffer to contain the reading from the module
-uint8_t buffer[14];
-uint8_t* buffer_at;
-uint8_t* buffer_end = buffer + sizeof(buffer);
- 
-void rfid_read(void);
-uint8_t rfid_get_next(void);
-unsigned long time;
-unsigned long lasttime;
-////////////////////////////////
 
 
-// variables will change:
+
+
+//Button stuff
+
+const int buttonPin_back = 3;     // the number of the pushbutton pin
+const int buttonPin_play = 4;     // the number of the pushbutton pin
+const int buttonPin_forward = 5;     // the number of the pushbutton pin
+const int ledPin =  13;      // the number of the LED pin
+
 int buttonState_back = 0;         // variable for reading the pushbutton status
 int buttonState_play = 0;         // variable for reading the pushbutton status
 int buttonState_forward = 0;         // variable for reading the pushbutton status
 int sentstate = 0;
+
+
+//RFID Stuff
+rdm630 rfid(2, 0);  //TX-pin of RDM630 connected to Arduino pin 2
+unsigned long time;
+unsigned long lastTime;
+unsigned long elapsedTime;
+unsigned long blocktimer;
+byte lastdata[6];
+bool block;
 
 void setup( ) {
     pinMode( A, INPUT );
@@ -110,23 +99,19 @@ void setup( ) {
     state = (digitalRead( A ) << 1) | digitalRead( B );     // Initialise state.
     old_count = 0;
 
+// initialize RFID
+    rfid.begin();
+    time = millis();
+    lastTime = millis();
+    blocktimer = 0L;
+
     Serial.begin( 9600 );
     Serial.println( "Jukebox Input Module Online" );
     
-    // Open software serial connection to RFID module
-    //pinMode(rfid_tx_pin,INPUT);
-    rfid.begin(9600);
- 
-    // Listen for interrupt from RFID module
-    attachInterrupt(rfid_irq,rfid_read,FALLING);
-    
-    lasttime = 0L;
-    time = 0L;
-    
-  
+
 }
 
-
+// Timer method for Rotary Encoder polling
 void T2_isr( ) {
 
     // Grab state of input pins.
@@ -192,104 +177,35 @@ void loop( ) {
     sentstate = 0;
   }
 
-//RFID
-  if ( ready )
-  {
-    // Convert the buffer into a 32-bit value
-    uint32_t result = 0;
-    
-    // Skip the preamble
-    ++buffer_at;
-    
-    // Accumulate the checksum, starting with the first value
-    uint8_t checksum = rfid_get_next();
-    
-    // We are looking for 4 more values
-    int i = 4;    
-    while(i--)
-    {
-      // Grab the next value
-      uint8_t value = rfid_get_next();
-      
-      // Add it into the result
-      result <<= 8;
-      result |= value;
-      
-      // Xor it into the checksum
-      checksum ^= value;
-    }
-    
-    // Pull out the checksum from the data
-    uint8_t data_checksum = rfid_get_next();
-    
-    // Print the result
-    //Serial.print("Reading: ");
-    //Serial.print(result);
- 
-    if ( checksum == data_checksum ) {
-      if (lasttime > 0L) {
-        time = millis();
-        digitalWrite(ledPin, HIGH); 
-        if ( time > (lasttime+2000L) ){
-          Serial.println(time);
-          Serial.println(lasttime);
-          Serial.println(result);
-          lasttime = millis();
+// RFID stuff
+    byte data[6];
+    byte length;
+
+    time = millis();
+
+    if(rfid.available()){
+        digitalWrite(ledPin, HIGH);
+       
+        if ((time - lastTime) < 1000) { // used to filter out sporadic spurious IDs
+          
+            rfid.getData(data,length);
+            // avoid continous repetition of one and the same ID while held to reader during a block time of 5 seconds
+            // output only if a different ID is read or if the block is removed
+            if (memcmp(lastdata,data,length) != 0 || (time-blocktimer > 5000)) { 
+              //shift the bytes to their respective positions
+              //explanation why casting is needed: http://www.mikrocontroller.net/topic/95708
+              unsigned long result = ((unsigned long int)data[1]<<24) + ((unsigned long int)data[2]<<16) + ((unsigned long int)data[3]<<8) + data[4];              
+              Serial.print("RFID: ");
+              Serial.println(result);
+              rfid.getData(lastdata,length);
+              blocktimer = millis();
+            }
         }
-        result = 0;
-        ready = false;
+        lastTime = millis();
+        delay(5);
         digitalWrite(ledPin, LOW); 
-      }
     }
-    else {
-      Serial.println(" CHECKSUM FAILED");
-      ready = false;
-    }
-    lasttime = lasttime+1L;
-     
-    // We're done processing, so there is no current value    
-    //ready = false;
-  }
-
-
 }
 
-//helper functions for RFID
-// Convert the next two chars in the stream into a byte and
-// return that
-uint8_t rfid_get_next(void)
-{
-  // sscanf needs a 2-byte space to put the result but we
-  // only need one byte.
-  uint16_t result;
- 
-  // Working space to assemble each byte
-  static char byte_chars[3];
-  
-  // Pull out one byte from this position in the stream
-  snprintf(byte_chars,3,"%c%c",buffer_at[0],buffer_at[1]);
-  sscanf(byte_chars,"%x",&result);
-  buffer_at += 2;
-  
-  return static_cast<uint8_t>(result);
-}
- 
-void rfid_read(void)
-{
-  // Only read in values if there is not already a value waiting to be
-  // processed
-  if ( ! ready )
-  {
-    // Read characters into the buffer until it is full
-    buffer_at = buffer;
-    while ( buffer_at < buffer_end )
-      *buffer_at++ = rfid.read();
-      
-    // Reset buffer pointer so it's easy to read out
-    buffer_at = buffer;
-  
-    // Signal that the buffer has data ready
-    ready = true;
-  }
-}
+
 
