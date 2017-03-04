9697ec5a95c8d684ecda6f9cc1c5d38f2bdb8f4f
alexfederlin <alexander.federlin@ericsson.com>
2017-03-04 23:49:05 +0100

    simplified playlistmapper

--- playlistmapper/playlistmapper.js
+++ playlistmapper/playlistmapper.js
@@ -1,3 +1,11 @@
+// playlistmapper
+/////////////////
+// this server handles the Database backend which stores the relation between 
+// the RFID tags and the playlist names
+// it exposes a REST interface which enables querying for a certain RFID tag.
+// The reply is provided in the form of a JSON String including the RFID tag
+// as well as the playlist name: {"rfid":"7616525","playlist":"plA"}
+
 var express = require('express')
 var app = express()
 var port = 4000
@@ -5,14 +13,10 @@ var port = 4000
 var Datastore = require('nedb')
   db = new Datastore({ filename: '/home/alex/Daten/Projekte/Development/jukebox/DBpopulator/database.db', autoload: true });
 
-
-app.get('/play/:rfid', RespondToPlay);
-
-
 app.get('/getplaylist/:rfid', RespondToGetPlaylist);
 
 app.listen(port, function () {
-  console.log('Example app listening on port '+port+'!')
+  console.log('Playlistmapper listening on port '+port+'!')
 })
 
 function RespondToGetPlaylist(req, res) {
@@ -27,54 +31,11 @@ function RespondToGetPlaylist(req, res) {
     }
     if ( typeof docs !== 'undefined' && docs ) {
       reply.playlist = docs.playlist
-      res.send(JSON.stringify(reply))
     }
     else {
-      res.send(JSON.stringify(reply))
+      console.log ("RFID tag not found in DB. ")
     }
+    res.send(JSON.stringify(reply))
+    console.log ("sending out reply: ",JSON.stringify(reply))
   })
-}
-
-function RespondToPlay (req, res) {
-  var reply =('Playing playlist associated with RFID tag '+req.params.rfid+'!')
-  db.findOne({"rfid":req.params.rfid}, function (err, docs){
-    if (err) {
-      console.error(err)
-      return
-    }
-    if ( typeof docs !== 'undefined' && docs ) {
-      res.send(reply + 'which is: '+docs.playlist)
-      playPlaylist(docs.playlist)
-    }
-    else {
-      res.send(reply + 'which is not defined in DB')
-    }
-  })
-}
-
-
-
-function playPlaylist(playlist){
-  var request = require('request');
-  var host ="localhost"
-  var port ="10081"
-  url = "http://localhost:10081/play";
-  requestData = { "entry" : playlist };
-  
-  console.log("start playing playlist "+playlist);
-  
-  request({
-      url: url,
-      method: "POST",
-      json: requestData
-    }, reportStatus);
-
-}
-
-function reportStatus (error, response, body) {
-    if (error){
-      console.log(error);
-      return;
-    }
-    console.log("starting to play: "+response.statusCode);
 }
