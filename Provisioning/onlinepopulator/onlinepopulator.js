8930d6c6afdc2937de28beaba3b189cdd4795626
alexfederlin <alexander.federlin@ericsson.com>
2017-09-15 08:59:28 +0200

    getting the onlinepopulator to work

--- Provisioning/onlinepopulator/onlinepopulator.js
+++ Provisioning/onlinepopulator/onlinepopulator.js
@@ -10,32 +10,28 @@ var subrfid = [];
 var datastore = 'database.db'
 var Datastore = require('nedb')
   , db = new Datastore({ filename: datastore, autoload: true });
+var itemsToBeProcessed = 0;
 
-//entry command, 
-//db.find({}, populateDbContents);
+//find amount of entries in DB which have any content in the "playpath" (i.e. exluding the playlist entries)
+db.count({playpath : /.*/ }, function (err, count) {
+  itemsToBeProcessed += count;
+});
+
 
 // entry command. Once all subdirs are found, run the "goThroughSubdirs callback"
 dir.subdirs("/home/alex/Musik/jukebox", goThroughSubdirs);
 
-// rea
-//function populateDbContents(err, docs) {  
-//}
 
 //for each subdir found, call "populateSubrfid"
 function goThroughSubdirs(err, subdirs) {
     if (err) throw err;
-    
-    //TODO: how to tell when all the calls are done?
     subdirs.forEach(populateSubrfid); 
-    
-    sortBy(subrfid, 'name')
-    subrfid.forEach(item => console.log(item.path + ' - ' + item.rfid))
-//    console.log('[' + subdirs + ']');
 }
+
 
 //define a new object with a path relative to jukebox folder
 //name is needed since the sorting algorithm sorts case sensitive, so make everything lower case to have alphabetical sorting
-function populateSubrfid(item, index) {
+function populateSubrfid(item, index, array) {
       // use let instead of var to have individual obj objects in the closure below
       let obj = {
         'name': item.toLowerCase().replace('/home/alex/musik/jukebox/',''),
@@ -47,15 +43,30 @@ function populateSubrfid(item, index) {
       // define a closure (I have no idea what I'm doing) as callbackfor the db.find call below
       function cl(err,docs){
         if (err) throw err;
-        if (docs.length == 0) return;
+        if (docs.length == 0) {
+            subrfid.push(obj);
+            return;
+        }
 //        console.log("MATCH!!! : "+docs[0].playpath);
 //        console.log("setting rfid: " + docs[0].rfid);
 //        console.log("on object:" + JSON.stringify(obj))
         obj.rfid = docs[0].rfid;
 //        console.log("result:" + JSON.stringify(obj))
-        // TODO: This does not work. I cannot modify the global subrfid from here, it seems
+
         subrfid.push(obj);
+
+        itemsToBeProcessed--;
+        console.log("items: " + itemsToBeProcessed)
+        if(itemsToBeProcessed === 0) {
+          createForm(subrfid);
+        }
       }
+
       db.find({playpath:obj.path}, cl);
+
 }
 
+function createForm (subrfid) {
+    sortBy(subrfid, 'name')
+    subrfid.forEach(item => console.log(item.path + ' - ' + item.rfid))
+}
