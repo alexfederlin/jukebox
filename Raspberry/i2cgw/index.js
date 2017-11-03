2c0a65d21968907f5c7cb2a9e477a001428707ff
Alexander Federlin <alexander@federlin.de>
2017-11-03 23:17:26 +0000

    i2c node adaptations

--- Raspberry/i2cgw/index.js
+++ Raspberry/i2cgw/index.js
@@ -1,5 +1,4 @@
 var i2c = require('i2c')
-    
 
 var ADDR = 0x0a
 var i2c1 = new i2c(ADDR, {device: '/dev/i2c-1'}); // point to your i2c address, debug provides REPL interface
@@ -11,25 +10,27 @@ function poll() {
     if (err) {
         console.log(err)
     }
-    if ((res == 48) | (res==97)){
-        if (previous != 48){
-            count += 1
-            console.log(count + ". pressed: "+res);
-            
-        }
-        previous=48;
+    count += 1
+    if (res == 2){
+      if (previous == 2){
+        console.log(count + ". pressed next");
+      }
+      previous = 2
     }
-    else {
-        if (previous != 0) {
-            console.log(count + ". released: "+res)
-        }
-        else {
-            //console.log ("idle "+res)
-        }
-        previous = 0;
+    if (res == 8){
+      console.log(count + ". pressed play");
+    }
+    if (res == 32){
+      console.log(count + ". pressed previous");
+    }
+    if (res == 0){
+      if (previous == 0) {
+        console.log(count + "released");
+      }
+      previous = 0
+    }
 
-    }
-    poll();
+    setTimeout(poll, 20);
   });
 };
 
