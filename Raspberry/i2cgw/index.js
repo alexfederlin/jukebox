67aae7252cbca4237f26aae4a4a26aef57b622d5
Alexander Federlin <alexander@federlin.de>
2017-11-03 23:34:23 +0000

    button presses on ATtiny sorted

--- Raspberry/i2cgw/index.js
+++ Raspberry/i2cgw/index.js
@@ -4,6 +4,7 @@ var ADDR = 0x0a
 var i2c1 = new i2c(ADDR, {device: '/dev/i2c-1'}); // point to your i2c address, debug provides REPL interface
 var previous = 0
 var count = 0
+var printed = 0
 
 function poll() {
   i2c1.readByte(function (err, res){
@@ -12,20 +13,24 @@ function poll() {
     }
     count += 1
     if (res == 2){
-      if (previous == 2){
+      if ((previous == 2) && (printed == 0)){
         console.log(count + ". pressed next");
+        printed = 1
       }
       previous = 2
     }
-    if (res == 8){
+    if ((res == 8) && (printed == 0)){
       console.log(count + ". pressed play");
+      printed = 1
     }
-    if (res == 32){
+    if ((res == 32) && (printed ==0)){
       console.log(count + ". pressed previous");
+      printed = 1
     }
     if (res == 0){
-      if (previous == 0) {
+      if ((previous == 0) && (printed == 1)) {
         console.log(count + "released");
+        printed = 0;
       }
       previous = 0
     }
@@ -33,6 +38,8 @@ function poll() {
     setTimeout(poll, 20);
   });
 };
+
+console.log ("starting");
 
 poll();
 
