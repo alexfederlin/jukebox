45730215c79ead77e3d7f846486dff53f5c2a20c
alexfederlin <alexander.federlin@ericsson.com>
2017-11-04 00:18:36 +0100

    changed ATtiny code

--- ATtiny/buttonctrl.ino
+++ ATtiny/buttonctrl.ino
@@ -1,19 +1,31 @@
 #include "TinyWire.h"
 
-#define led_pin 1
-#define button_pin 3
-#define error_led_pin 4
+// read these as PB1, PB3, PB4
+#define button_pl 1
+#define button_pr 3
+#define button_ne 4
 
 byte own_address = 10;
-bool button_pressed = false;
+
+
+#define PR 5
+#define PL 3
+#define NE 1
+
+// bit map: pr pl ne
+// nothing pressed: 000000
+// only pr pressed: 100000
+// only pl pressed: 001000
+// only ne pressed: 000010
+// all three pressed: 101010
+// excellent explanation of the bitshifting here: https://embeddedthoughts.com/2016/05/25/getting-started-with-the-attiny85/#more-1173
+byte buttonstate = 0x0;
 
 void setup() {
-  // config led_pin as Output for driving an LED
-  pinMode(led_pin, OUTPUT);
-  // config error_led_pin as Output for driving an LED
-  pinMode(error_led_pin, OUTPUT);
-  // config button_pin als INPUT for a connected button (normally open; connects to GND)
-  pinMode( button_pin, INPUT_PULLUP);
+  // config button pins as INPUT for a connected button (normally open; connects to GND)
+  pinMode( button_pl, INPUT_PULLUP);
+  pinMode( button_pr, INPUT_PULLUP);
+  pinMode( button_ne, INPUT_PULLUP);
   
   // config TinyWire library for I2C slave functionality
   TinyWire.begin( own_address );
@@ -22,13 +34,25 @@ void setup() {
 }
 
 void loop() {
-  if(digitalRead(button_pin) == 0) {
-    button_pressed = true;
-    digitalWrite(error_led_pin, LOW);
+  if(digitalRead(button_pl) == 0) {
+    buttonstate |= (1 << PL);
   }
   else {
-    button_pressed = false;
-    digitalWrite(led_pin, LOW);
+    buttonstate &= ~(1 << PL);
+  }
+
+  if(digitalRead(button_pr) == 0) {
+    buttonstate |= (1 << PR);
+  }
+  else {
+    buttonstate &= ~(1 << PR);
+  }
+
+  if(digitalRead(button_ne) == 0) {
+    buttonstate |= (1 << NE);
+  }
+  else {
+    buttonstate &= ~(1 << NE);
   }
 
 }
@@ -36,12 +60,5 @@ void loop() {
 // Request Event handler function
 //  --> Keep in mind, that this is executed in an interrupt service routine. It shouldn't take long to execute
 void onI2CRequest() {
-  if (button_pressed == true) {
-      TinyWire.send('a');  
-      digitalWrite(led_pin, HIGH);
-  }
-  else {
-    TinyWire.send('b');
-    digitalWrite(error_led_pin, HIGH);
-  }
+  TinyWire.send(buttonstate);
 }
