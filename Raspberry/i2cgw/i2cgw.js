b2a0c493345298af570bf1532e2d5ddb48233ccf
Alexander Federlin <alexander@federlin.de>
2018-01-22 18:34:01 +0000

    added RFID functionality to i2cgw

--- Raspberry/i2cgw/i2cgw.js
+++ Raspberry/i2cgw/i2cgw.js
@@ -1,36 +1,51 @@
 var i2c = require('i2c-bus'),
   i2c1;
 
+
+// I2C Addresses
 var ADDR_BTN = 0x0a
 var ADDR_RTRY = 0x0b
 var ADDR_LCD = 0x3f
+var ADDR_RFID = 0x14
 
+// open I2C and scan
+i2c1 = i2c.openSync(1);
+i2c1.scan(cb);
 
+// RFID Stuff
+var buffer = new ArrayBuffer(4); // buffer holding just the RFID Tag
+var uint8 = new Uint8Array(7); // buffer holding RFID Tag + delimiter sequence 0 255 0
+var uint8_analyze = new Uint8Array(buffer); //8bit int view on the RFID tag (needs to be reordered)
+var uint32_analyze = new Uint32Array(buffer); // 32 bit int view on the RFID Tag
+var lastRFID=0;
+
+// Volume Stuff
 var prev_vol = 0
 var volume = 0
 
+// Button Stuff
 var previous = 0
 var count = 0
 var printed = 0
 var blipcount = 0
 
-
-i2c1 = i2c.openSync(1);
-
-i2c1.scan(cb);
-
-
+// LCD Stuff
 var LCD = require('lcdi2c');
 var lcd = new LCD( 1, ADDR_LCD, 16, 2 );
 lcd.clear();
 lcd.on();
 
 
+
+
 function cb(err,devices){
   if (err){
     console.log(err)
   }
-  console.log(devices);
+  console.log("Devices on the i2c bus:");
+  devices.forEach (function (device) {
+    console.log("0x"+device.toString(16));
+  });
 }
 
 function cbBtn(err,res){
@@ -43,7 +58,7 @@ function cbBtn(err,res){
      console.log(count + ". pressed next");
      try{
        lcd.clear()
-       lcd.printlnBlock( 'Next');
+       lcd.println( 'Next');
      } catch (err) {
        console.log(err)
      }
@@ -57,7 +72,7 @@ function cbBtn(err,res){
    printed = 1
    try{
      lcd.clear()
-     lcd.printlnBlock( 'Play');
+     lcd.println( 'Play');
    } catch (err) {
      console.log(err)
    }
@@ -69,7 +84,7 @@ function cbBtn(err,res){
    printed = 1
    try{
      lcd.clear()
-     lcd.printlnBlock( 'Previous');
+     lcd.println( 'Previous');
    } catch (err) {
      console.log(err)
    }
@@ -94,7 +109,7 @@ function cbRtry(err,res){
         console.log(volume);
         try {
           lcd.clear()
-          lcd.printlnBlock( "Volume: " + volume );
+          lcd.println( "Volume: " + volume );
         } catch (err) {
           console.log(err)
         }
@@ -111,12 +126,56 @@ function cbRtry(err,res){
     //setTimeout(poll, 500);
 };
 
+function cbRFID(){
+
+  // we are reversing the order of the 4 byte array which holds the RFID tag
+  // then we interpret the whole array as one big number, which happens to be
+  // exactly the RFID Tag we are after
+  uint8_analyze.reverse();
+
+  // only display RFID tag if it changed
+  if (lastRFID != uint32_analyze[0]) {
+    console.log("RFID: " + uint32_analyze[0]);
+    lcd.clear()
+    lcd.println( "RFID: " + uint32_analyze[0] );
+    lastRFID = uint32_analyze[0];
+  }
+}
 
 
 function poll() {
-  i2c1.receiveByte(ADDR_BTN, cbBtn)
-  i2c1.receiveByte(ADDR_RTRY, cbRtry)
-  setTimeout(poll, 50);
+  try{
+    i2c1.receiveByte(ADDR_RTRY, cbRtry)
+    i2c1.receiveByte(ADDR_BTN, cbBtn)
+
+  }
+  catch(err){
+    console.log("issue with reading from buttons or rotary enccoder: "+err)
+  }
+// read RFID
+  // every time we get something new from the i2c bus, we basically shift
+  // the array and put the new value at the end. As there is no shift and
+  // push with typed arrays, we have to do it manually
+  uint8.copyWithin(0,1); // shift
+  try {
+    uint8[uint8.length-1] = i2c1.receiveByteSync(ADDR_RFID); // push
+  }
+  catch(err){
+    //console.log("issue with reading from RFID: "+err);
+  }
+  //console.log("data: " + uint8[0] + " " + uint8[1] + " " + uint8[2] + " " + uint8[3] + " " + uint8[4] + " " + uint8[5] + " " + uint8[6])
+
+  // the ATTiny is sending a delimiter sequence of 0 255 0 between RFID tags
+  // only if we detect this delimiter sequence at the beginning of the array
+  // we will trigger the analyzing function.
+  if ((uint8[0]==0)&&(uint8[1] == 255) && (uint8[2] == 0)) {
+    for (var j=0; j<uint8_analyze.length; j++){
+      uint8_analyze[j] = uint8[j+3];
+    }
+    cbRFID();
+  }
+
+  setTimeout(poll, 20);
 }
 
 poll();
