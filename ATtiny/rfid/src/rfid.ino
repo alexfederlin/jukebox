e793b6290c5ae9e3aae53143e46abb2cf6984bd2
alexfederlin <alexander.federlin@ericsson.com>
2018-02-21 22:05:37 +0100

    some cleanup in ATTiny code for RFID

--- ATtiny/rfid/src/rfid.ino
+++ ATtiny/rfid/src/rfid.ino
@@ -7,42 +7,43 @@
 // return a bitmap of the last read RFID (hopefully)
 //#####################################################
 
-
-byte own_address = 20;
-
 int ledPin = 1;
 
 rdm630 rfid(3, 4);  //TX-pin of RDM630 connected to PB3
+
 unsigned long time;
 unsigned long lastTime;
-unsigned long elapsedTime;
 unsigned long blocktimer;
+
 byte data[6];
 byte lastdata[6];
-unsigned long lastRFID = 0;
+
+byte i2cAddress = 20;
 int i2cSendStep = 0;
 bool i2cSendComplete = true;
 byte i2cSendData[6];
-bool block;
+
+
 
 void setup() {
 
   pinMode(ledPin, OUTPUT);
   digitalWrite(ledPin, LOW);
+
 // initialize RFID
   rfid.begin();
+
   time = millis();
   lastTime = millis();
   blocktimer = 0L;
-    
+
   // config TinyWire library for I2C slave functionality (SDA: PB0 , SCL: PB2)
-  TinyWireS.begin( own_address );
+  TinyWireS.begin( i2cAddress );
   // register a handler function in case of a request from a master
   TinyWireS.onRequest( onI2CRequest );
 }
 
 void loop() {
-// RFID stuff
     
     byte length;
 
@@ -50,43 +51,32 @@ void loop() {
 
     if(rfid.available()){
         
-        if ((time - lastTime) < 1000) { // used to filter out sporadic spurious IDs
+        if ((time - lastTime) < 1000) { // throw away first read ID. Used to filter out sporadic spurious IDs
 
             rfid.getData(data,length);
             // avoid continous repetition of one and the same ID while held to reader during a block time of 5 seconds
             // output only if a different ID is read or if the block is removed
             if (memcmp(lastdata,data,length) != 0 || (time-blocktimer > 5000)) { 
               digitalWrite(ledPin, HIGH);  
-              //shift the bytes to their respective positions
-              //explanation why casting is needed: http://www.mikrocontroller.net/topic/95708
-              lastRFID = ((unsigned long int)data[1]<<24) + ((unsigned long int)data[2]<<16) + ((unsigned long int)data[3]<<8) + data[4];              
-          //    Serial.print("RFID: ");
-          //    Serial.println(result);
-              //lcdDisplay(String(result));
-
               rfid.getData(lastdata,length);
               blocktimer = millis();
-          //    lcdRaise();
             }
         }
         lastTime = millis();
-//        delay(5);
         digitalWrite(ledPin, LOW); 
     }
+    // copy the latest read data to the send buffer only if we are not in the
+    // process of currently sending out an RFID
+    
     if (i2cSendComplete) {
       for (int i=0; i<6; i++) {
         i2cSendData[i] = data[i];
       }
-      
     }
 }
 
 // Request Event handler function
 //  --> Keep in mind, that this is executed in an interrupt service routine. It shouldn't take long to execute
-// void onI2CRequest() {
-//   TinyWireS.send(lastRFID);
-// }
-
 void onI2CRequest()
 {
 
